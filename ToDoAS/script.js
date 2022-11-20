bee7a98eb5d0779079b2d884ea3d49c7d7b20266
Shimis <78961147+Shinigamiixd@users.noreply.github.com>
2022-11-20 19:33:46 +0200

    Add files via upload

---
+++ ToDoAS/script.js
@@ -0,0 +1,9 @@
+let todo_name = document.querySelector(".todo_name")
+let todo_priority = document.querySelector(".todo_priority")
+let todo_duedate = document.querySelector(".todo_duedate")
+let todo_status = document.querySelector(".todo_status")
+let todo_submit = document.querySelector(".todo_submit")
+
+todo_submit.addEventListener("click", addTodo)
+
+function addTodo(){}
